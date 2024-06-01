993fe4fb06e88c58963a928421a10c83b58eec23
KevinSantiagoSS <kserrano7@udi.edu.co>
2024-06-01 10:33:51 -0500

    Main, Sesion y MP documentados.

--- proyecto/src/application/Main.java
+++ proyecto/src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
-	
+
+/**
+ * Importa las clases necesarias.
+ */
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -7,8 +10,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+/**
+ * La clase Main es la clase principal que inicia la aplicación.
+ * Esta clase carga el archivo FXML de la pantalla de inicio de sesión y lo muestra en la ventana principal de la aplicación.
+ * @author Kevin Santiago
+ *
+ */
 public class Main extends Application {
+	
+	/**
+	 * Método principal que inicia la aplicación.
+	 * Carga y muestra la interfaz de usuario de inicio de sesión.
+	 * @param primaryStage El escenario principal de la aplicación.
+	 */
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -23,7 +37,11 @@ public class Main extends Application {
 		}
 	}
 	
-	
+	/**
+	 * Método principal que inicia la aplicación.
+	 * Llama al método launch() de la clase Application para iniciar la aplicación JavaFX.
+	 * @param args Los argumentos de la línea de comandos.
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
